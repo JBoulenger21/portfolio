9898971e59c4aec9ce527c94a284195f2c973feb
JBoulenger21 <boulenger.julie@gmail.com>
2020-09-09 17:03:13 +0200

    en cours de centrer la navbar de gauche

--- index.php
+++ index.php
@@ -1,21 +1,39 @@
 <?php
 include('header.php');
 ?>
-<header>
+<!-- <header>
   <div class="container">
     <h1 class="text-center">Julie BOULENGER - Developpeuse web</h1>
   </div>
-</header>
+</header> -->
 
   <div class="row m-0">
-    <div class="col-1 sidebarleft mx-auto p-0">
-      <p class="mb-0 d-flex">Sidebar</p>
+    <div class="col-1 sidebar sidebarleft mx-auto p-0">
+      <div class="mb-0">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link " href="#">Accueil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#">A propos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#">Compétences</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#">Projets</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
     </div>
       <div class="content col-10 p-0">
         <div class="container">
           <div class="main">
-            <h1>Main</h1>
-            <h2>Bienvenue !</h2>
+            <h1 class="text-center">PORTFOLIO</h1>
+            <h2>Venez découvrir mon univers !</h2>
             <p>Bonjour, je m'appelle Julie BOULENGER, et je vais vous faire découvrir mon parcours...</p>
             <h3>A propos</h3>
             <p>Blablabla</p>
@@ -25,25 +43,29 @@ include('header.php');
           </div>
         </div>
       </div>
-      <div class="col-1 sidebarright mx-auto p-0">
-        <p>Sidebar</p>
+      <div class="col-1 sidebar sidebarright mx-auto p-0">
+        <ul>
+          <li>LinkedIn</li>
+          <li>GitHub</li>
+          <li>CV</li>
+        </ul>
       </div>
   </div>
 
 <div class="container">
 
-<h2>Contact</h2>
+<h2 id="contact">Contact</h2>
 
 <form>
   <div class="mb-3">
     <label for="exampleFormControlInput1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required>
   </div>
   <div class="mb-3">
     <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" required></textarea>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Me contacter</button>
 </form>
 </div>
 
