175f72baa1f5268cfb1a11042bd99f728d6ff0ce
JBoulenger21 <boulenger.julie@gmail.com>
2020-09-16 10:50:54 +0200

    sidebar convenable

--- index.php
+++ index.php
@@ -8,25 +8,31 @@ include('header.php');
 </header> -->
 
   <div class="row m-0">
-    <div class="sidebar sidebarleft ">
-      <div class="">
-        <ul class="nav">
-          <li class="nav-item">
-            <a class="nav-link " href="#">Accueil</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link " href="#">A propos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link " href="#">Compétences</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link " href="#">Projets</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link " href="#contact">Contact</a>
-          </li>
-        </ul>
+    <div class="sidebar sidebarleft">
+      <div>
+        <div class="item">
+            <a href="#">Accueil</a>
+        </div>
+      </div>
+      <div>
+        <div class="item">
+            <a href="#">A<span>_</span>propos</a>
+        </div>
+      </div>
+      <div>
+        <div class="item">
+            <a href="#">Compétences</a>
+        </div>
+      </div>
+      <div>
+        <div class="item">
+            <a href="#">Projets</a>
+        </div>
+      </div>
+      <div>
+        <div class="item">
+            <a href="#contact">Contact</a>
+        </div>
       </div>
     </div>
       <div class="content p-0">
@@ -34,21 +40,26 @@ include('header.php');
           <div class="main">
             <h1 class="text-center">PORTFOLIO</h1>
             <h2>Venez découvrir mon univers !</h2>
-            <p>Bonjour, je m'appelle Julie BOULENGER, et je vais vous faire découvrir mon parcours...</p>
+            <p>Bonjour, je m'appelle Julie BOULENGER, et je vais vous faire découvrir mon parcours...
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <h3>A propos</h3>
-            <p>Blablabla</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <h3>Compétences</h3>
-            <p>...</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
           </div>
         </div>
       </div>
-      <div class="sidebar sidebarright mx-auto p-0">
-        <ul>
-          <li><a href="https://www.linkedin.com/in/julieboulenger21/" target="_blank">LinkedIn</a></li>
-          <li><a href="https://github.com/JBoulenger21" target="_blank">GitHub</a></li>
-          <li><a href="ressources/CV_Julie_BOULENGER_Dev.pdf" download="CV Julie Boulenger" target="_blank">CV</a></li>
-        </ul>
+      <div class="sidebar sidebarright">
+        <div>
+            <div class="item"><a href="https://www.linkedin.com/in/julieboulenger21/" target="_blank">LinkedIn</a></div>
+        </div>
+        <div>
+            <div class="item"><a href="https://github.com/JBoulenger21" target="_blank">GitHub</a></div>
+        </div>
+        <div>
+            <div class="item"><a href="ressources/CV_Julie_BOULENGER_Dev.pdf" download="CV Julie Boulenger" target="_blank">CV</a></div>
+        </div>
       </div>
   </div>
 
