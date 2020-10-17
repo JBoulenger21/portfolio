644621e1f4cc7c219a3d59538dc8ee9d2cf6f1c0
JBoulenger21 <boulenger.julie@gmail.com>
2020-10-17 14:42:35 +0200

    avancée de la page projet

--- projet.php
+++ projet.php
@@ -1,26 +1,32 @@
 <?php
 include('header.php');
 ?>
-
-<div class="container">
-  <h2>Projet "Explorateur de fichiers"</h2>
-  <h3>Projet réalisé en groupe - Avec Laurine Herard.</h3>
-  <img class="imgprojet" src="ressources/explofichiers.PNG" alt="Aperçu explorateur de fichiers">
+<div class="container titreProjet">
+  <h1>Mes projets</h1>
 </div>
-<div class="container">
+<div class="container pageprojet">
   <h2>Projet "Explorateur de fichiers"</h2>
   <h3>Projet réalisé en groupe - Avec Laurine Herard.</h3>
   <img class="imgprojet" src="ressources/explofichiers.PNG" alt="Aperçu explorateur de fichiers">
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
 </div>
-<div class="container">
+<div class="container pageprojet">
   <h2>Projet "Explorateur de fichiers"</h2>
   <h3>Projet réalisé en groupe - Avec Laurine Herard.</h3>
   <img class="imgprojet" src="ressources/explofichiers.PNG" alt="Aperçu explorateur de fichiers">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
 </div>
-<div class="container">
+<div class="container pageprojet">
   <h2>Projet "Explorateur de fichiers"</h2>
   <h3>Projet réalisé en groupe - Avec Laurine Herard.</h3>
   <img class="imgprojet" src="ressources/explofichiers.PNG" alt="Aperçu explorateur de fichiers">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+</div>
+<div class="container pageprojet">
+  <h2>Projet "Explorateur de fichiers"</h2>
+  <h3>Projet réalisé en groupe - Avec Laurine Herard.</h3>
+  <img class="imgprojet" src="ressources/explofichiers.PNG" alt="Aperçu explorateur de fichiers">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
 </div>
 
 <?php
